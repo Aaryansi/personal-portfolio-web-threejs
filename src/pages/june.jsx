63ce16ec946938b863538f3807c93298ef6863b9
Aaryan Singh <singha9@rose-hulman.edu>
2025-07-01 06:01:48 -0400

    v5

--- src/pages/june.jsx
+++ src/pages/june.jsx
@@ -9,7 +9,7 @@ const June = () => (
       i am very sorry for everything! i have infinte amount of faith & trust in you and i am sure u will contact me. we all do dumb things and then forgive eachother, i am sure we will too if theres anything at all. <br />
 
 i don’t want you to think i just disappeared.i don’t even know why things blew up or what your parents have been telling you about me. She called me and said u two are talking marriage so end this right here. I dont understand whats wrong in being friends as we have been for the last year. Breaking this up has already started taking a lot of toll on my health and im sure it must have happened to you as well and career.
-all i’m asking is you to say that we are okay. Be it directly to me or any of mine or ur friends. Rmr my chats with ahaan where i talked about you?
+all i’m asking is you to say that we are okay. Be it directly to me or any of mine or ur friends. Rmr my chats with my friend where i talked about you?
 
 
 — aaryan
