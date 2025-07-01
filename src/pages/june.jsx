c689bee9e92314174da111c2509ee0ae98d63e69
Aaryan Singh <singha9@rose-hulman.edu>
2025-07-01 05:47:35 -0400

    v3

--- src/pages/june.jsx
+++ src/pages/june.jsx
@@ -5,7 +5,7 @@ const June = () => (
   <div className="h-screen flex flex-col items-center justify-center bg-black text-white px-4">
     <h1 className="text-6xl font-bold">June 6</h1>
     <p className="mt-1 text-l max-w-md text-center">
-      hi shravani, very important - i am very sick for the last 3 days and at the hospital. After talking to your mom my bp shot up causing some internal damage, it’s critical right now. I have been given steroids and sedations initially to control but my bp isn’t going down and it's getting worse. If your phone is with you, please find a way to text me, please! i need it to get better. we dont need to make this big and take it to ur parents again.
+      hi shravani, very important - i am very sick for the last 3 days. After talking to your mom my bp shot up but my bp isn’t going down and it's getting worse. If your phone is with you, please find a way to text me, please! i need it to get better. we dont need to make this big and take it to ur parents again.
       i am very sorry for everything! i have infinte amount of faith & trust in you and i am sure u will contact me. we all do dumb things and then forgive eachother, i am sure i will too if theres anything. <br />
 
 i don’t want you to think i just disappeared.i don’t even know why things blew up or what your parents have been telling you about me. She called me and said u two are talking marriage so end this right here. I dont understand whats wrong in being friends as we have been for the last year. Breaking this up has already started taking a lot of toll on my health and im sure it must have happened to you as well and career.
